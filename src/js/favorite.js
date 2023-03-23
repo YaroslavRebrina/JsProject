b18f67ca5d2f15a6dcb20579845d9fd1ee5bb629
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-03-23 11:44:22 +0200

    added favorite page functional

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,13 +1,35 @@
 import { FAVORITE_KEY } from './constants';
+import favoriteItemMarkup from '../templates/favorites.hbs';
 
-localStorage.setItem(FAVORITE_KEY, '1');
+const refs = {
+  listRef: document.querySelector('.favorite__list'),
+};
+
+const { listRef } = refs;
+
+localStorage.setItem(
+  FAVORITE_KEY,
+  JSON.stringify([
+    {
+      img: 'src',
+      title: 'title',
+      text: 'text',
+      date: 'date',
+      linl: 'link',
+    },
+  ])
+);
 
 function getFavorites() {
   const favoriteItems = localStorage.getItem(FAVORITE_KEY);
-  return JSON.stringify(favoriteItems);
+  return JSON.parse(favoriteItems);
 }
 
-function favoriteMarkup() {
-  const items = getFavorites();
-  console.log(items);
+function favoriteMarkup(callback) {
+  const items = callback;
+
+  let markup = items.map(favoriteItemMarkup).join('');
+  listRef.insertAdjacentHTML = markup;
 }
+
+favoriteMarkup(getFavorites());
