48bae80f91e2ef5985f859a6894e5b694dc1130b
M. A. Startsev <m.a.startsev@gmail.com>
2023-03-25 07:33:41 +0200

    add obj-normalize func

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './js/cards-service';
 import './js/search-form';
 
 import test from './js/card/test'; //генерація тестової картки
-
+import test from './js/obj-normalize/test';
 import './js/constants';
 import './js/favorite';
