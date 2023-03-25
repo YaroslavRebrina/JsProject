0abc1cf59df40a2c898d143d4f2761274e5fd579
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2023-03-26 00:57:41 +0200

    add switcher theme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './js/obj-normalize/cards-servise-corr';
 import './js/obj-normalize/search-form-corr';
+import './js/switcher';
 
 // import test from './js/card/test'; //генерація тестової картки
 // import { renderNormalize } from './js/obj-normalize/render-normalize';
