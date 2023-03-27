ebd9b329180d874fd1d0d565aba96c8aaaf0e0bd
LiudmylaShevchuk <lashevchuk12@gmail.com>
2023-03-27 17:12:21 +0300

    flatpickr, index.js, calendar.html, _calendar.scss added

--- src/js/calendar/index.js
+++ src/js/calendar/index.js
@@ -1,14 +1,44 @@
-import CalendarDates from 'calendar-dates';
-const calendarDates = new CalendarDates();
+// import CalendarDates from 'calendar-dates';
+// const calendarDates = new CalendarDates();
 
-const main = async () => {
-  for (const meta of await calendarDates.getDates(new Date())) {
-    console.log(meta);
-  }
+// const main = async () => {
+//   for (const meta of await calendarDates.getDates(new Date())) {
+//     console.log(meta);
+//   }
 
-  for (const meta of await calendarDates.getMatrix(new Date())) {
-    console.log(meta);
-  }
-};
+//   for (const meta of await calendarDates.getMatrix(new Date())) {
+//     console.log(meta);
+//   }
+// };
 
-main();
+// main();
+
+// document.getElementById('date'),
+//   addEventListener('DOMContentLoaded', () => {
+//     document.getElementById('calendar-container').hidden = false;
+//     const calendar = new VanillaCalendar('#calendar-container');
+//     calendar.init();
+//   });
+
+// new VanillaCalendar('#calendar-container', {
+//   DOMTemplates: {
+//     default: `
+//       <div class="vanilla-calendar-header">
+//         <#ArrowPrev />
+//         <div class="vanilla-calendar-header__content">
+//           <#Month />
+//           <#Year />
+//         </div>
+//         <#ArrowNext />
+//       </div>
+//       <div class="vanilla-calendar-wrapper">
+//         <#WeekNumbers />
+//         <div class="vanilla-calendar-content">
+//           <#Week />
+//           <#Days />
+//         </div>
+//       </div>
+//       <#ControlTime />
+//     `,
+//   },
+// });
