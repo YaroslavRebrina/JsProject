b2aaec116868578a84282f1b88f2d8048ef0eea1
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-03-27 22:23:57 +0300

    commit

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -2,6 +2,7 @@ import { FAVORITE_KEY, FAVORITE_TOTAL, RENDERED } from './constants';
 import { dataForFavorite } from './pagination/paginationByPopular';
 import { card } from './card';
 
+import sprite from '../fonts/images/icons.svg';
 const refs = {
   listRef: document.querySelector('.card__list'),
   buttonRef: document.querySelector('.card__favorite'),
@@ -12,50 +13,69 @@ const { listRef, buttonRef, favListRef } = refs;
 let favouriteNews = JSON.parse(localStorage.getItem(FAVORITE_KEY)) || [];
 // let colletionTotal = JSON.parse(localStorage.getItem(FAVORITE_TOTAL)) || 0;
 
-listRef.addEventListener('click', addToFavourites);
+listRef.addEventListener('click', toggleFavorite);
 // listRef.addEventListener('click', removeFavourite);
 
 favoriteMarkup(getFavorites());
 
 function addToFavourites(evt) {
-  if (evt.target.nodeName === 'BUTTON' || 'USE' || 'SPAN') {
-    console.log(evt.target);
-    const addedToFavorite = JSON.parse(localStorage.getItem(RENDERED)).find(
-      item => item.id === evt.target.closest('li').id
-    );
+  console.log(evt.target);
+  const addedToFavorite = JSON.parse(localStorage.getItem(RENDERED)).find(
+    item => item.id === evt.target.closest('li').id
+  );
 
-    addedToFavorite.isFavorite = true;
-    favouriteNews.push(addedToFavorite);
-    localStorage.setItem(FAVORITE_KEY, JSON.stringify(favouriteNews));
-    console.log(favouriteNews);
+  evt.target.closest('button').innerHTML = `<span> Remove from favorite
+        <svg width="16" height="16" class="box__icon remove-icon">
+          <use href="${sprite}#icon-like-active"></use>
+        </svg>
+      </span>`;
+  evt.target.classList.add('liked');
+  addedToFavorite.isFavorite = true;
+  favouriteNews.push(addedToFavorite);
+  localStorage.setItem(FAVORITE_KEY, JSON.stringify(favouriteNews));
+  console.log(favouriteNews);
+  return;
+}
+
+function toggleFavorite(evt) {
+  if (
+    evt.target.nodeName === 'BUTTON' &&
+    !evt.target.classList.contains('liked')
+  ) {
+    addToFavourites(evt);
+  } else {
+    removeFavourite(evt);
   }
 }
 
 function removeFavourite(evt) {
-  if (evt.target.nodeName === 'BUTTON') {
-    if (dataForFavorite === null) {
-      // dataForFavorite = ;
-    }
-    const addedToFavorite = JSON.parse(localStorage.getItem(RENDERED)).find(
-      item => item.id === evt.target.closest('li').id
-    );
-    addedToFavorite.isFavorite = false;
-    favouriteNews.filter(item => !addedToFavorite);
-    localStorage.setItem(FAVORITE_KEY, JSON.stringify(favouriteNews));
-    console.log(favouriteNews);
+  const addedToFavorite = JSON.parse(localStorage.getItem(RENDERED)).find(
+    item => item.id === evt.target.closest('li').id
+  );
+  evt.target.closest('button').innerHTML = `<span>
+      Add to favorite
+        <svg  width="16" heigth="16">
+          <use href="${sprite}#icon-like-nonactive"></use>
+        </svg>
+      </span>`;
+  evt.target.classList.remove('liked');
+  addedToFavorite.isFavorite = false;
+  favouriteNews.filter(item => !addedToFavorite);
+  localStorage.setItem(FAVORITE_KEY, JSON.stringify(favouriteNews));
+  console.log(favouriteNews);
+  if (document.body.classList.contains('.favorite'))
     favoriteMarkup(getFavorites());
-  }
 }
 
 //auto markup
 
-function getFavorites() {
+export function getFavorites() {
   try {
     return favouriteNews;
   } catch (e) {}
 }
 
-function favoriteMarkup(callback) {
+export function favoriteMarkup(callback) {
   const items = callback.map(item => card(item)); //markup
 
   try {
