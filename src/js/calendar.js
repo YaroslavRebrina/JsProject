b9b4f0b20f438cd8bd73c8e71f668b59dc025b9e
LiudmylaShevchuk <lashevchuk12@gmail.com>
2023-03-28 22:37:13 +0300

    html, js, json changed

---
+++ src/js/calendar.js
@@ -0,0 +1,9 @@
+config = {
+  dateFormat: 'd/m/Y',
+  defaultDate: '24/02/2022',
+  noCalendar: false,
+  // clickOpens: false,
+  // wrap: true,
+  // altInput: true,
+};
+flatpickr('input[type=datetime-local]', config);
