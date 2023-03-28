1781c6f7b6bc8f7f19268f8755ebb88b98bd3270
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-03-28 19:46:41 +0300

    header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-// import './js/cards-service';
-// import './js/categories';
+import './js/cards-service';
+import './js/categories';
 
-// import './js/switcher';
+import './js/switcher';
 
-// import './js/constants';
-// import './js/favorite';
+import './js/constants';
+import './js/favorite';
 // import './js/pagination/paginationBySearch';
 // import './js/pagination/paginationByPopular';
 // import './js/pagination/panginationByCategory';
+
+import './js/header';
