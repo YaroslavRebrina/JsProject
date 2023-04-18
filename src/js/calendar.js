e78617924d33c7f5fc5943cbef93380b867abdc3
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-04-18 23:12:38 +0300

    fixed input coloring and some small calendar fix

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -7,9 +7,9 @@ const FLATPICKR_INPUT = document.querySelector('.flatpickr-input');
 const ARROW_BTN_DOWN = document.querySelector('.arrow-down');
 const ARROW_BTN_UP = document.querySelector('.arrow-up');
 const CALENDAR_ICON = document.querySelector('.calendar__button--left');
-const CARDS_LIST = document.querySelector('.cards__list');
+const CARDS_LIST = document.querySelector('.card__list');
 const ICONS_URL = './img/icons.svg';
-const pag = document.querySelector('.pagination');
+const containerPagination = document.querySelector('.pagination');
 
 let requestDate;
 let filterDate;
@@ -38,10 +38,7 @@ const DATEPICKER_OPTIONS = {
         clearActiveBtn();
       } else {
         // console.log('categories-in-calendar', cardsApiService);
-        const filtredArticles = filterByDateCategory(
-          filterDate,
-          cardsApiService
-        );
+        const filtredArticles = filterByDateCategory(filterDate, favouriteNews); /////////////////////////cardsApiService === favouriteNews
         renderFiltredMarkupCategory(filtredArticles);
       }
       requestDate = formatRequestDate(dateObj);
@@ -94,10 +91,10 @@ function filterByDateCategory(filterDate, articlesArray) {
 
 function renderFiltredMarkupPopular(filtredArticles) {
   if (filtredArticles.length === 0) {
-    const img = new URL('#');
+    const img = './images/tablet/tablet.png';
     const markupWithNotFoundImg = `<div class="no-news"><p class="no-news__text">There are no news for this date. </p><img class="no-news__img" src="${img}" alt="No news found"></div>`;
     CARDS_LIST.innerHTML = markupWithNotFoundImg;
-    pag.style = 'display: none';
+    containerPagination.style = 'display: none';
   } else {
     const list = filtredArticles
       .map(item => favoriteMarkup(getFavorites(item)))
@@ -108,7 +105,7 @@ function renderFiltredMarkupPopular(filtredArticles) {
 
 function renderFiltredMarkupCategory(filtredArticles) {
   if (filtredArticles.length === 0) {
-    const img = new URL('#');
+    const img = './images/tablet/tablet.png';
     const markupWithNotFoundImg = `<div class="no-news"><p class="no-news__text">There are no news from this category for this date. Try searching by key word</p><img class="no-news__img" src="${img}" alt="No news found"></div>`;
     CARDS_LIST.innerHTML = markupWithNotFoundImg;
     containerPagination.style = 'display: none';
